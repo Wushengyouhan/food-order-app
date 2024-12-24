0fbb8d5dff1bd64548c29e00431d8fba56f520f4
Wushengyouhan <840835067@qq.com>
2024-12-24 21:49:33 +0800

    288. Finishing & Using the Cart Context & Reducer

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,77 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+  items: [],
+  addItem: () => {},
+  removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    const existingCartItenIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    // 复制一份
+    const updateItems = [...state.items];
+    if (existingCartItenIndex > -1) {
+      const existingItem = updateItems[existingCartItenIndex];
+      const updateItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updateItems[existingCartItenIndex] = updateItem;
+    } else {
+      updateItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updateItems };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    const existingCartItenIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingItem = state.items[existingCartItenIndex];
+    const updateItems = [...state.items];
+
+    if (existingItem.quantity === 1) {
+      updateItems.splice(existingCartItenIndex, 1);
+    } else {
+      const updateItem = {
+        ...existingItem,
+        quantity: existingItem.quantity - 1,
+      };
+      updateItems[existingCartItenIndex] = updateItem;
+    }
+
+    return { ...state, items: updateItems };
+  }
+
+  return state;
+}
+
+export function CartContextProvider({ children }) {
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
+}
+
+export default CartContext;
